ef9361db54e8df4187e9a6e1e158563ab0818a51
Jan Henrik Overland <janhenrik.overland@gmail.com>
2016-11-02 12:04:30 +0100

    svg export form

--- src/ui/DownloadButtonItems.js
+++ src/ui/DownloadButtonItems.js
@@ -4,27 +4,25 @@ DownloadButtonItems = function(refs, layout) {
     var uiManager = refs.uiManager,
         i18n = refs.i18nManager.get();
 
-    //return [
-        //{
-            //xtype: 'label',
-            //text: i18n.graphics,
-            //style: 'padding:7px 5px 5px 7px; font-weight:bold'
-        //},
-        //{
-            //text: i18n.image_png + ' (.png)',
-            //iconCls: 'ns-menu-item-image',
-            //handler: function() {
-                //uiManager.submitSvgForm('png');
-            //}
-        //},
-        //{
-            //text: 'PDF (.pdf)',
-            //iconCls: 'ns-menu-item-image',
-            //handler: function() {
-                //uiManager.submitSvgForm('pdf');
-            //}
-        //}
-    //];
-
-    return [];
+    return [
+        {
+            xtype: 'label',
+            text: i18n.graphics,
+            style: 'padding:7px 5px 5px 7px; font-weight:bold'
+        },
+        {
+            text: i18n.image_png + ' (.png)',
+            iconCls: 'ns-menu-item-image',
+            handler: function() {
+                uiManager.submitSvgForm('png');
+            }
+        },
+        {
+            text: 'PDF (.pdf)',
+            iconCls: 'ns-menu-item-image',
+            handler: function() {
+                uiManager.submitSvgForm('pdf');
+            }
+        }
+    ];
 };
