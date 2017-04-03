43c798bd6633ea7902412f2b03011e8dbb6dc9dd
Edoardo Sabadelli <edoardo@dhis2.org>
2017-04-03 11:17:55 +0200

    Attempt to avoid restarting dev server when rebuilding dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,14 @@ var webpack = require('webpack');
 var webpackBaseConfig = require('./webpack-base-config');
 
 webpackBaseConfig.devServer = {
-    progress: true,
-    colors: true,
     port: 8081,
     inline: true,
+
+    // delay build a bit to avoid errors when
+    // dependencies files are not built yet
+    watchOptions: {
+        aggregateTimeout: 1000
+    }
 };
 
 module.exports = webpackBaseConfig;
