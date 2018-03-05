e9856802bf896a8d2fda8c64159143f603260cc1
Jan Henrik Øverland <jan@dhis2.org>
2018-03-05 18:00:59 +0100

    Add error message to plugin call

--- src/plugin.js
+++ src/plugin.js
@@ -149,7 +149,7 @@ function render(plugin, layout) {
         });
     }
     else {
-        instanceManager.getReport(new api.Layout(instanceRefs, layout));
+        instanceManager.getReport(new api.Layout(instanceRefs, layout), false, false, false, null, { noError: true, errorMessage: 'No data to display' });
     }
 }
 
