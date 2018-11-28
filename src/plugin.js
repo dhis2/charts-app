b5fa80a844dd2a942244d038fad793faaa8a6a31
Edoardo Sabadelli <edoardo@dhis2.org>
2018-11-28 15:19:13 +0100

    YOY support in chart plugin (#55)

--- src/plugin.js
+++ src/plugin.js
@@ -10,20 +10,19 @@ import { api, manager, config, init, util } from 'd2-analysis';
 
 import { Layout } from './api/Layout';
 
+import { init as d2init, getInstance } from 'd2';
+
 // extend
 api.Layout = Layout;
 
 // references
 var refs = {
     api,
-    init
+    init,
 };
 
 // inits
-var inits = [
-    init.legendSetsInit,
-    init.dimensionsInit
-];
+var inits = [init.legendSetsInit, init.dimensionsInit];
 
 // dimension config
 var dimensionConfig = new config.DimensionConfig();
@@ -43,7 +42,7 @@ refs.chartConfig = chartConfig;
 
 // app manager
 var appManager = new manager.AppManager(refs);
-appManager.apiVersion = 29;
+appManager.apiVersion = 31;
 refs.appManager = appManager;
 
 // calendar manager
@@ -73,6 +72,91 @@ periodConfig.init();
 appManager.applyTo(arrayTo(api));
 optionConfig.applyTo(arrayTo(api));
 
+// generic period names without year for xAxis labels
+function computeGenericPeriodNames(responses) {
+    const xAxisRes = responses.reduce((out, res) => {
+        if (out.metaData) {
+            if (res.metaData.dimensions.pe.length > out.metaData.dimensions.pe.length) {
+                out = res;
+            }
+        } else {
+            out = res;
+        }
+
+        return out;
+    }, {});
+
+    const metadata = xAxisRes.metaData;
+
+    return metadata.dimensions.pe.reduce((genericPeriodNames, periodId) => {
+        const name = metadata.items[periodId].name;
+
+        // until the day the backend will support this in the API:
+        // trim off the trailing year in the period name
+        // english names should all have the year at the end of the string
+        genericPeriodNames.push(name.replace(/\s+\d{4}$/, ''));
+
+        return genericPeriodNames;
+    }, []);
+}
+
+// Year over year special handling
+function apiFetchAnalyticsForYearOverYear(current, options) {
+    d2init({ baseUrl: appManager.getApiPath() });
+
+    var d2;
+    var yearlySeriesLabels = [];
+
+    return getInstance()
+        .then(d2Instance => {
+            d2 = d2Instance;
+        })
+        .then(() => {
+            let yearlySeriesReq = new d2.analytics.request()
+                .addPeriodDimension(current.yearlySeries)
+                .withSkipData(true)
+                .withSkipMeta(false)
+                .withIncludeMetadataDetails(true);
+
+            if (options.relativePeriodDate) {
+                yearlySeriesReq = yearlySeriesReq.withRelativePeriodDate(
+                    options.relativePeriodDate
+                );
+            }
+
+            return d2.analytics.aggregate.fetch(yearlySeriesReq);
+        })
+        .then(yearlySeriesRes => {
+            const requests = [];
+
+            const now = new Date();
+            const currentDay = ('' + now.getDate()).padStart(2, 0);
+            const currentMonth = ('' + (now.getMonth() + 1)).padStart(2, 0);
+
+            yearlySeriesRes.metaData.dimensions['pe'].forEach(period => {
+                yearlySeriesLabels.push(yearlySeriesRes.metaData.items[period].name);
+
+                const startDate = `${period}-${currentMonth}-${currentDay}`;
+
+                const req = new d2.analytics.request()
+                    .fromModel(current)
+                    .withParameters(options)
+                    .withRelativePeriodDate(startDate);
+
+                requests.push(d2.analytics.aggregate.get(req));
+            });
+
+            return Promise.all(requests);
+        })
+        .then(responses => {
+            return Promise.resolve({
+                responses: responses.map(res => new d2.analytics.response(res)),
+                yearlySeriesLabels,
+            });
+        })
+        .catch(e => console.log(e));
+}
+
 // plugin
 function render(plugin, layout) {
     if (!util.dom.validateTargetDiv(layout.el)) {
@@ -98,19 +182,21 @@ function render(plugin, layout) {
     // instance manager
     uiManager.setInstanceManager(instanceManager);
 
-    instanceManager.setFn(function (_layout) {
+    // options passed to createChart
+    var extraOptions = {};
+
+    instanceManager.setFn(function(_layout) {
         if (!util.dom.validateTargetDiv(_layout.el)) {
             return;
         }
 
-        var fn = function (legendSetId) {
+        var fn = function(legendSetId) {
             var el = _layout.el;
             var element = document.getElementById(el);
             var response = _layout.getResponse();
-            var extraOptions = {
-                legendSet: appManager.getLegendSetById(legendSetId),
-                dashboard: instanceManager.dashboard
-            };
+
+            extraOptions.legendSet = appManager.getLegendSetById(legendSetId);
+            extraOptions.dashboard = instanceManager.dashboard;
 
             var { chart } = createChart(response, _layout, el, extraOptions);
 
@@ -118,16 +204,16 @@ function render(plugin, layout) {
             uiManager.reg(chart, 'chart');
 
             // dashboard item resize
-            element.setViewportWidth = function (width) {
-                chart.setSize(width, undefined, {duration: 50});
+            element.setViewportWidth = function(width) {
+                chart.setSize(width, undefined, { duration: 50 });
             };
 
-            element.setViewportHeight = function (height) {
-                chart.setSize(undefined, height, {duration: 50});
+            element.setViewportHeight = function(height) {
+                chart.setSize(undefined, height, { duration: 50 });
             };
 
-            element.setViewportSize = function (width, height) {
-                chart.setSize(width, height, {duration: 50});
+            element.setViewportSize = function(width, height) {
+                chart.setSize(width, height, { duration: 50 });
             };
 
             // mask
@@ -136,11 +222,10 @@ function render(plugin, layout) {
 
         // legend set
         if (_layout.doLegendSet()) {
-            appManager.getLegendSetIdByDxId(_layout.getFirstDxId(), function (legendSetId) {
+            appManager.getLegendSetIdByDxId(_layout.getFirstDxId(), function(legendSetId) {
                 fn(legendSetId);
             });
-        }
-        else {
+        } else {
             fn();
         }
     });
@@ -150,18 +235,46 @@ function render(plugin, layout) {
     }
 
     if (layout.id) {
-        instanceManager.getById(layout.id, function (_layout) {
+        instanceManager.getById(layout.id, function(_layout) {
             _layout = new api.Layout(instanceRefs, objectApplyIf(layout, _layout));
 
             if (!util.dom.validateTargetDiv(_layout.el)) {
                 return;
             }
 
-            instanceManager.getReport(_layout);
+            // special handling for YEAR_OVER_YEAR chart types
+            if (_layout.type.match(/^YEAR_OVER_YEAR/i)) {
+                const options = {};
+
+                if (_layout.aggregationType && _layout.aggregationType !== 'DEFAULT') {
+                    options.aggregationType = _layout.aggregationType;
+                }
+
+                if (_layout.completedOnly) {
+                    options.completedOnly = _layout.completedOnly;
+                }
+
+                apiFetchAnalyticsForYearOverYear(_layout, options).then(
+                    ({ responses, yearlySeriesLabels }) => {
+                        // set responses in layout object so getReport does not perform analytics requests
+                        _layout.setResponse(responses);
+
+                        // extra options to pass to createChart() special for YOY chart types
+                        extraOptions.yearlySeries = yearlySeriesLabels;
+                        extraOptions.xAxisLabels = computeGenericPeriodNames(responses);
+
+                        instanceManager.getReport(_layout);
+                    }
+                );
+            } else {
+                instanceManager.getReport(_layout);
+            }
         });
-    }
-    else {
-        instanceManager.getReport(new api.Layout(instanceRefs, layout), false, false, false, null, { noError: true, errorMessage: 'No data to display' });
+    } else {
+        instanceManager.getReport(new api.Layout(instanceRefs, layout), false, false, false, null, {
+            noError: true,
+            errorMessage: 'No data to display',
+        });
     }
 }
 
